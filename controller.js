c0384511cb97df73539424598c7b365a601f80c9
Eveline Yunita <evelineyunita@outlook.com>
2020-04-02 16:21:14 +0700

    berhasil tambah data

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ exports.index = function(req,res){
 
 //menampilkan semua data mahasiswa
 exports.tampilsemuadatasiswa = function(req,res){
-    connection.query('SELECT *FROM mahasiswa', function(error, rows, fileds){
+    connection.query('SELECT *FROM mahasiswa', function(error, rows, fields){
         if(error){
             console.log(error);
         }else{
@@ -22,11 +22,28 @@ exports.tampilsemuadatasiswa = function(req,res){
 exports.tampilberdasarkanid = function(req, res){
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-    function(error, rows, fileds){
+    function(error, rows, fields){
         if(error){
             console.log(error);
         }else{
             response.ok(rows, res);
         }
     });
-}
+};
+
+//menambahkan data mahasiswa
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)',
+        [nim, nama, jurusan],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data!", res)
+            }
+        });
+};
